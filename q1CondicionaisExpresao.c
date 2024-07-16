19b2b7c23af112e2f3d94cf879555b96bcd8a4cc
rauana-carvalho <136634083+rauana-carvalho@users.noreply.github.com>
2024-07-16 09:31:59 -0300

    Create q1CondicionaisExpresao.c

---
+++ q1CondicionaisExpresao.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int a, b;
+
+    printf("digite o valor de a: ");
+    scanf("%d", &a);
+
+    printf("digite o valor de b: ");
+    scanf("%d", &b);
+
+    if (a == b){
+        puts("a eh igual a b");
+    }
+    else if (a > b) {
+        puts("a eh maior do que b");
+    }
+    else if (a < b) {
+        puts("b eh maior do que a");
+    }
+    else{
+        puts("valor invalido");
+    }
+    printf("a=%d e b=%d", a, b);
+
+    return 0;
+}
